91f2a373510109d103b55e1e491e717b9db5c0a8
Updews Prado <updews.prado@gmail.com>
2017-09-06 15:28:35 +0800

    + Created initial "Test Case" object + Created "read.php" for reading all test cases

---
+++ api/objects/testcase.php
@@ -0,0 +1,125 @@
+<?php
+class Testcase {
+
+	// database connection and table name
+	private $conn;
+	private $table_name = "test_cases";
+
+	// object properties
+	public $id;
+	public $name;
+	public $desc;
+	public $globalWait;
+	public $steps;
+
+	// constructor with $db as database connection
+	public function __construct($db) {
+		$this->conn = $db;
+	}
+
+	public function readAll() {
+		// echo "Read Test Suites table \n";
+		$query = "SELECT * FROM " . $this->table_name;
+		$result = $this->conn->query($query);
+
+		return $result;
+	}
+
+	// public function create() {
+	// 	// echo "Create a Test Suite entry \n";
+	// 	$query = "INSERT INTO " . $this->table_name . " SET ts_name=?, ts_desc=?";
+
+	// 	// prepare query
+	// 	$stmt = $this->conn->stmt_init();
+	// 	$stmt = $this->conn->prepare($query);
+
+	// 	// sanitize
+	// 	$this->name = htmlspecialchars(strip_tags($this->name));
+	// 	$this->desc = htmlspecialchars(strip_tags($this->desc));
+
+	// 	// bind values
+	// 	$stmt->bind_param('ss', $this->name, $this->desc);
+
+	// 	// execute query
+	// 	if($stmt->execute()){
+	// 		return true;
+	// 	}
+	// 	else{
+	// 		return false;
+	// 	}
+	// }
+
+	// // Used when viewing only one test suite or for updating
+	// public function readOne() {
+	// 	// read single record
+	// 	$query = "SELECT ts_id, ts_name, ts_desc FROM " . $this->table_name . " WHERE ts_id=? LIMIT 1";
+	// 	// echo "$query";
+
+	// 	// prepare query
+	// 	$stmt = $this->conn->stmt_init();
+	// 	$stmt = $this->conn->prepare($query);
+
+	// 	//bind id of test suite to be updated
+	// 	$stmt->bind_param("i", $this->id);
+
+	// 	// execute query
+	// 	$stmt->execute();
+	// 	// bind result variables
+	// 	$stmt->bind_result($ts_id, $ts_name, $ts_desc);
+	// 	// fetch row
+	// 	$stmt->fetch();
+
+	// 	// set values to object properties
+	// 	$this->id = $ts_id;
+	// 	$this->name = $ts_name;
+	// 	$this->desc = $ts_desc;
+	// }
+
+	// public function update() {
+	// 	// update query
+	// 	$query = "UPDATE " . $this->table_name . " SET ts_name=?, ts_desc=? WHERE ts_id=? LIMIT 1";
+
+	// 	// prepare query
+	// 	$stmt = $this->conn->stmt_init();
+	// 	$stmt = $this->conn->prepare($query);
+
+	// 	// sanitize
+	// 	$this->id = htmlspecialchars(strip_tags($this->id));
+	// 	$this->name = htmlspecialchars(strip_tags($this->name));
+	// 	$this->desc = htmlspecialchars(strip_tags($this->desc));
+
+	// 	// bind values
+	// 	$stmt->bind_param('ssi', $this->name, $this->desc, $this->id);
+
+ //    // execute query
+ //    if($stmt->execute()){
+ //      return true;
+ //    }
+ //    else{
+ //      return false;
+ //    }
+	// }
+
+	// public function delete() {
+	// 	// delete query
+	// 	$query = "DELETE FROM " . $this->table_name . " WHERE ts_id=?";
+
+	// 	// prepare query
+	// 	$stmt = $this->conn->stmt_init();
+	// 	$stmt = $this->conn->prepare($query);
+
+	// 	// sanitize
+	// 	$this->id = htmlspecialchars(strip_tags($this->id));
+
+	// 	// bind values
+	// 	$stmt->bind_param('i', $this->id);
+
+	// 	// execute query
+ //    if($stmt->execute()){
+ //      return true;
+ //    }
+ //    else{
+ //      return false;
+ //    }
+	// }
+}
---
+++ api/testcase/read.php
@@ -0,0 +1,49 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/testcase.php';
+
+// instantiate database and testcase object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$testcase = new testcase($db);
+
+// query test_cases
+$results = $testcase->readAll();
+$num = $results->num_rows;
+
+// check if more than 0 record found
+if ($num > 0) {
+	// test_cases array
+	$test_cases_arr = array();
+	$test_cases_arr["records"] = array();
+
+  while ($row = $results->fetch_assoc()) {
+  	extract($row);
+		$test_case_item = array(
+			"tc_id" => $tc_id,
+			"tc_name" => $tc_name,
+			"tc_desc" => $tc_desc,
+			"global_wait" => $global_wait,
+			"steps" => $steps
+		);
+
+		array_push($test_cases_arr["records"], $test_case_item);
+  }
+
+  echo json_encode($test_cases_arr);
+} 
+else {
+  echo json_encode(
+    array("message" => "No products found.")
+  );
+}
+
+
+?>
