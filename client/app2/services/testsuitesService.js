522132ef8cdd555b69cccd479547a7532ae31920
Updews Prado <updews.prado@gmail.com>
2017-11-09 15:54:37 +0800

    - "testsuitesService.js", rearranged code pattern for the service file for easier readability

--- client/app2/services/testsuitesService.js
+++ client/app2/services/testsuitesService.js
@@ -1,38 +1,46 @@
-angular.module('beatsApp').service('TestsuitesService', function($http, $log) {
-	var service = {
-		getAllTestsuites: getAllTestsuites,
-		getTestsuiteDetail: getTestsuiteDetail,
-		createTestsuite: createTestsuite,
-		deleteTestsuite: deleteTestsuite,
-	}
+(function() {
+  'use strict';
 
-	return service;
+	angular
+		.module('beatsApp')
+		.service('TestsuitesService', function($http, $log) {
+			var service = {
+				getAllTestsuites: getAllTestsuites,
+				getTestsuiteDetail: getTestsuiteDetail,
+				createTestsuite: createTestsuite,
+				deleteTestsuite: deleteTestsuite,
+			}
 
-	function getAllTestsuites() {
-		return $http.get('api/testsuite/read.php').then(function(resp) {
-			$log.debug("TestsuitesService getAllTestsuites", resp.data);
-			return resp.data;
+			return service;
+
+			function getAllTestsuites() {
+				return $http.get('api/testsuite/read.php').then(function(resp) {
+					$log.debug("TestsuitesService getAllTestsuites", resp.data);
+					return resp.data;
+				});
+			}
+
+			function getTestsuiteDetail(ts_id) {
+				return $http.post('api/testsuite/read.php', { id: ts_id }).then(function(resp) {
+					$log.debug("TestsuitesService getTestsuiteDetail", resp.data);
+					return resp.data;
+				});
+			}
+
+			function createTestsuite(params) {
+				return $http.post('api/testsuite/create.php', { name: params.ts_name, desc: params.ts_desc }).then(function(resp) {
+					$log.debug("TestsuitesService createTestsuite", resp.data);
+					return resp.data;
+				});
+			}
+
+			function deleteTestsuite(params) {
+				// $log.debug("TestsuitesService deleteTestsuite", params);
+				return $http.post('api/testsuite/delete.php', { id: params.ts_id }).then(function(resp) {
+					$log.debug("TestsuitesService deleteTestsuite", resp.data);
+					return resp.data;
+				});
+			}
 		});
-	}
 
-	function getTestsuiteDetail(ts_id) {
-		return $http.post('api/testsuite/read.php', { id: ts_id }).then(function(resp) {
-			$log.debug("TestsuitesService getTestsuiteDetail", resp.data);
-			return resp.data;
-		});
-	}
-
-	function createTestsuite(params) {
-		return $http.post('api/testsuite/create.php', { name: params.ts_name, desc: params.ts_desc }).then(function(resp) {
-			$log.debug("TestsuitesService createTestsuite", resp.data);
-			return resp.data;
-		});
-	}
-
-	function deleteTestsuite(params) {
-		return $http.post('api/testsuite/delete.php', { id: params.ts_id }).then(function(resp) {
-			$log.debug("TestsuitesService deleteTestsuite", resp.data);
-			return resp.data;
-		});
-	}
-});
+})();
