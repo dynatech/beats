3ead000635eaed21bef1c71f03b56c72d54bf7b2
Updews Prado <updews.prado@gmail.com>
2017-11-16 16:24:21 +0800

    - Test Case CRUD Integration a. Remove step functionality is working

--- client/app2/components/testcase.js
+++ client/app2/components/testcase.js
@@ -21,10 +21,29 @@
 		$log.debug("testcaseController start");
 		var vm = this;
 
+		vm.crud_status = null;
+		vm.params = null;
+    vm.selected = null;
+
 		// Function declarations
+		vm.clearParams = clearParams;
+		vm.clickBeatsAction = clickBeatsAction;
 		vm.addStep = addStep;
+		vm.removeStep = removeStep;
 
 		// Function definitions
+
+		function clearParams() {
+			$log.debug("testcaseController | clearParams");
+			vm.params = null;
+			vm.crud_status = null;
+		}
+
+		function clickBeatsAction(params) {
+			$log.debug("testcaseController | clickBeatsAction", params);
+			vm.params = params;
+		}
+
 		function addStep() {
 			$log.debug("testcaseController | addStep: start");
 
@@ -45,6 +64,12 @@
 			vm.tcdata.testcases[0].steps.push(step_blank);			
 			$log.debug("testcaseController | addStep: end", vm.tcdata.testcases[0].steps);
 		}
+
+		function removeStep(index) {
+			$log.debug("testcaseController | removeStep: ", index);
+			vm.tcdata.testcases[0].steps.splice(index, 1);
+		}
+
 	}
 
 })();
