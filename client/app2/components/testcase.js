d5a11f972c8bdf3e916ef1767e930da0c09a57d9
Updews Prado <updews.prado@gmail.com>
2017-11-16 15:58:30 +0800

    - Test Case CRUD Integration a. Basic "addStep()" functionality for adding a test case step

--- client/app2/components/testcase.js
+++ client/app2/components/testcase.js
@@ -20,6 +20,31 @@
 	function testcaseController($log, $scope, $http, $window, TestcasesService) {
 		$log.debug("testcaseController start");
 		var vm = this;
+
+		// Function declarations
+		vm.addStep = addStep;
+
+		// Function definitions
+		function addStep() {
+			$log.debug("testcaseController | addStep: start");
+
+			var step_blank = {
+				name: '',
+				action: 'Choose action',
+				locateElement: {},
+				element_id: {},
+				op_special_1: {},
+				op_special_2: {},
+				assert_options: {}
+			};
+
+			if (vm.tcdata.testcases[0].steps == null) {
+				vm.tcdata.testcases[0].steps = [];
+			}
+
+			vm.tcdata.testcases[0].steps.push(step_blank);			
+			$log.debug("testcaseController | addStep: end", vm.tcdata.testcases[0].steps);
+		}
 	}
 
 })();
